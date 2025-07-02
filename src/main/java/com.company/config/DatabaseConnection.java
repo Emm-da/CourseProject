0e6736237f837659ebf5c5a25146575c511a5cef
Emm-da <ann4zhgutova@yandex.ru>
2025-07-02 21:57:31 +0300

    Update and rename DatabaseConnection to DatabaseConnection.java

---
+++ src/main/java/com.company/config/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package com.company.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private static final String url = "jdbc:mysql://localhost:3306/order_management?useSSL=false&serverTimezone=UTC";
+    private static final String username = "Anna";
+    private static final String password = "123"; // пароль, который вы используете в phpMyAdmin
+
+
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Database connection established");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+}
